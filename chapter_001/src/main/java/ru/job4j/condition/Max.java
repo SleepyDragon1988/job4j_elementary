34cb0d2dd66120cddbb5457922cc412dd162026c
SleepyDragon1988 <Ilya175@mail.ru>
2020-04-02 01:12:24 +0300

    chapter001

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -2,6 +2,14 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int result = left;
-        return result;    }
+        boolean yes = left>right;
+        int result = yes ? left : right;
+        return result;
+    }
+
+}
+
+public static void main(String[] args) {
+    String msg = Max.max(9, 4);
+    System.out.println("максимальное из введеных значений равно ", msg);
 }
