2dcc02e3b477f8d940be087dd7220aa216811328
SleepyDragon1988 <Ilya175@mail.ru>
2020-04-02 02:33:23 +0300

    Otladka

---
+++ src/ru/job4j/condition/Otladka.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+public class Otladka {
+
+    public static boolean exist(double ab, double ac, double bc) {
+
+        if ((ab + ac > bc) && (ac + bc > ab) && (ab + bc > ac) == true) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        boolean yes = Otladka.exist(8.0, 2.0, 2.0);
+        String vyvod = yes? "треугольник с заданным сторонами существует" : "треугольник с заданным сторонами не существует";
+        System.out.println(vyvod);
+    }
+}
