fccc73f85eee91ea88a06a2b8dff0400dd8cdce1
SleepyDragon1988 <Ilya175@mail.ru>
2020-04-02 02:22:11 +0300

    4.3.1 существует ли треугольник

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -10,6 +10,6 @@ public class Max {
 }
 
 public static void main(String[] args) {
-    String msg = Max.max(9, 4);
+    int msg = Max.max(9, 4);
     System.out.println("максимальное из введеных значений равно ", msg);
 }
