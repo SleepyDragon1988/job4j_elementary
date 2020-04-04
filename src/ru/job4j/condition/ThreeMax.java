76c5825f3449256ea315379e99342cd299063def
SleepyDragon1988 <Ilya175@mail.ru>
2020-04-05 02:01:17 +0300

    ThreeMax

---
+++ src/ru/job4j/condition/ThreeMax.java
@@ -0,0 +1,31 @@
+package ru.job4j.condition;
+
+public class ThreeMax{
+        public static int max(int first, int second, int third) {
+            int result = first;
+            if (first > second && first > third) {
+                result = second;
+            }
+            if (second > first && second > third) {
+                result = third;
+            }
+            return result;
+        }
+        public static void main(String [] args){
+            int mx1 = ThreeMax.max(10, 5, 3);
+            System.out.println(mx1);
+            int mx2 = ThreeMax.max(10, 50, 1);
+            System.out.println(mx2);
+            int mx3 = ThreeMax.max(1, 5, 100);
+            System.out.println(mx3);
+            int mx4 = ThreeMax.max(1,1,1);
+            System.out.println(mx4);
+            int mx5 = ThreeMax.max(10, 10, 1);
+            System.out.println(mx5);
+            int mx6 = ThreeMax.max(100,1, 100);
+            System.out.println(mx6);
+            int mx7 = ThreeMax.max(1, 100, 100);
+            System.out.println(mx7);
+
+        }
+}
