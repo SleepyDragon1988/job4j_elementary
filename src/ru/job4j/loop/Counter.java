d7e016c6d7793f27e4e37fe10cc30bd1de3d218a
SleepyDragon1988 <Ilya175@mail.ru>
2020-04-05 02:40:18 +0300

    sumByEven

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -3,8 +3,18 @@ package ru.job4j.loop;
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
-        for ( int index = start; index <= finish; index++ ) {
-            sum = sum + index;
+        for ( int i = start; i<=finish; i++ ) {
+            sum = sum + i ;
+        }
+        return sum;
+    }
+
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for ( int in = start; in<=finish; in++  ) {
+            if ( in%2==0 ) {
+                sum = sum + in ;
+            }
         }
         return sum;
     }
@@ -13,5 +23,9 @@ public class Counter {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
+
+        System.out.println(sumByEven(0, 10));
+        System.out.println(sumByEven(3, 8));
+        System.out.println(sumByEven(1, 1));
     }
 }
