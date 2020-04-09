b681e1b33b6aea0fa2d31ef8316beb35d05a0546
SleepyDragon1988 <Ilya175@mail.ru>
2020-04-10 00:35:38 +0300

    converter +javadoc

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,15 +1,36 @@
 package ru.job4j.converter;
 
+/**
+ * @author Илья
+ * @since 2020
+ * @version 1
+ */
 public class Converter {
+    /**
+     * деление
+     * @param value значение в рублях
+     * @return деление рублей на курс евро
+     */
     public static int rubletoEuro(int value){
         int rsl = value/70;
         return rsl;
     }
+
+    /**
+     *
+     * @param value
+     * @param value значение в рублях
+     * @return деление рублей на курс доллара
+     */
     public static int rubletoDollar(int value){
         int rsl = value/60;
         return rsl;
     }
 
+    /**
+     * умножение
+     * @param args вызов методов rubletoDollar и rubletoEuro и передача значений параметрам
+     */
     public static void main(String [] args){
         int in = 140;
         int expected = 2;
