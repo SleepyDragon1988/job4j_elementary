229d0d99bea5d4cafd96abda4a99b64da5f8c7c3
SleepyDragon1988 <Ilya175@mail.ru>
2020-04-09 16:57:51 +0300

    Mortage

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(int amount, int salary, double percent) {
+        int year = 0;
+
+        while (amount > 0) {
+            amount = (int) (amount * percent / 100 - salary);
+            year++;
+        }
+        return year;
+    }
+
+    public static void main(String[] args) {
+        int rls = year(100, 70, 50);
+        System.out.println(rls);
+    }
+}
