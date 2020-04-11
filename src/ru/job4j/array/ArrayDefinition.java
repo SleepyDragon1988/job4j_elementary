2e009dc62ea7d4d9acdecf1f42bc2b0327011053
SleepyDragon1988 <Ilya175@mail.ru>
2020-04-12 00:50:36 +0300

    array2

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,19 @@ public class ArrayDefinition {
         short[] age = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        /**
+         * второе задание
+         */
+        String[] names = new String[4];
+        names[0]="1";
+        names[1]="2";
+        names[2]="3";
+        names[3]="4";
+
+        System.out.println(names);
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
